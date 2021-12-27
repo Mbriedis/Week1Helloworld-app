d7a2ef2f352a78ade94cca9bd9a96c996ec5e3b7
Martins Briedis <mbriedis@tktk.ee>
2021-12-27 18:30:43 +0200

    Create Hello World

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Who are you?");
+string UserName;
+UserName = Console.ReadLine ();
+Console.Read();
+Console.WriteLine("Hello " + UserName);
+Console.Read ();
